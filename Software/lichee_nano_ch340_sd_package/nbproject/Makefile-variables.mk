da2316e44f552a46b84fd4d236aa97a0a911c040
pecostm32 <53696654+pecostm32@users.noreply.github.com>
2022-01-23 19:18:57 +0100

    Add files via upload

---
+++ Software/lichee_nano_ch340_sd_package/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU_ARM-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU_ARM-Linux
+CND_ARTIFACT_NAME_Debug=lichee_nano_ch340_sd_package.elf
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU_ARM-Linux/lichee_nano_ch340_sd_package.elf
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU_ARM-Linux/package
+CND_PACKAGE_NAME_Debug=licheenanoch340sdpackage.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU_ARM-Linux/package/licheenanoch340sdpackage.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=lichee_nano_ch340_sd_package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/lichee_nano_ch340_sd_package
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=licheenanoch340sdpackage.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/licheenanoch340sdpackage.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
